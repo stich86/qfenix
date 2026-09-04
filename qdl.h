9cb451f81515fb13faf47caf4a192ec802abf3f3
Giammarco <stich86@gmail.com>
2026-09-04 23:55:09 +0200

    Add attr_as_unsigned_default function declaration
    Signed-off-by: Giammarco <stich86@gmail.com>

--- qdl.h
+++ qdl.h
@@ -160,6 +160,8 @@ int decode_programmer(char *s, struct sahara_image *images);
 void sahara_images_free(struct sahara_image *images, size_t count);
 void print_hex_dump(const char *prefix, const void *buf, size_t len);
 unsigned int attr_as_unsigned(xmlNode *node, const char *attr, int *errors);
+unsigned int attr_as_unsigned_default(xmlNode *node, const char *attr,
+				      unsigned int def);
 const char *attr_as_string(xmlNode *node, const char *attr, int *errors);
 bool attr_as_bool(xmlNode *node, const char *attr, int *errors);
 
